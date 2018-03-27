926bbac8957190c434188f79c6ba96d0b6d42c17
chipallino1 <skorupich00@mail.ru>
2018-03-27 08:06:32 +0300

    ver

--- src/Main.java
+++ src/Main.java
@@ -26,17 +26,17 @@ public class Main {
             Boing boing = boingFactory.create();
             PilotBoing pilotBoing = boingFactory.addPilot();
 
+
             boing.getDiscription();
             pilotBoing.letFly();
-
             AirCompany.ILFactory ilFactory = new AirCompany().new ILFactory();
             IL il = ilFactory.create();
             PilotIL pilotIL = ilFactory.addPilot();
 
             il.getDiscription();
-            pilotIL.letFly();
 
-            Plane[] planes = new Plane[5];
+            pilotIL.letFly();
+            Plane[] planes = new Plane[5];//коллекция
             planes[0] = new Plane("Plane 1", "me", 12, 123);
             planes[1] = new Plane("Plane 2", "me", 123, 13);
             planes[2] = new Plane("Plane 3", "me", 2, 12);
@@ -46,6 +46,7 @@ public class Main {
             Dispatcher dispatcher = Dispatcher.createDispatcher();
             dispatcher.searchFuel(planes);
             dispatcher.sortByLong(planes);
+
 
             for (int i = 0; i < planes.length; i++) {
                 System.out.println(planes[i].getLongOfVoyage() + " " + planes[i].getName());
